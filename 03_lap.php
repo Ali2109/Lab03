00f2da55724a78dacd870c8121c4430c15c0af5f
Ali <ali.alblewi1403@gmail.com>
2022-10-13 02:20:18 +0300

    Create 03_lap.php

---
+++ 03_lap.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Lab03</title>
+</head>
+<body>
+    <?php
+
+$x=15;  
+$y=30;  
+$z=$x+$y;  
+echo "Sum: ",$z;  
+
+
+$x=100;  
+$y=90;  
+$z=$x-$y;  
+echo "<br> Subtract:",$z;  
+
+
+$x=10;  
+$y=60;  
+$z=$x*$y;  
+echo "<br> multiply:",$z;  
+
+$x=100;  
+$y=50;  
+$z=$x/$y;  
+echo "<br> Dividing:",$z;
+
+
+?>
+
+</body>
+</html>
